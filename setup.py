0c6536e7c4a64c35463863682cb4ae60110700ed
Jorge Henrique Piazentin Ono <jorgehpo@nyu.edu>
2021-04-12 01:43:31 -0400

    adding setup and manifest

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="notebookjs",
+    version="0.1.1",
+    author="Jorge Piazentin Ono, Juliana Freire, Claudio Silva",
+    author_email="jorgehpo@nyu.edu",
+    description="notebookJS library - easy bidirectional communication between python and javascript",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jorgehpo/notebookJS",
+    packages=find_packages(exclude=['resources']),
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "notebook"
+    ]
+)
