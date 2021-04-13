f2c5b439b89323291701cdc75286d406da222c69
Jorge Henrique Piazentin Ono <jorgehpo@nyu.edu>
2021-04-12 23:23:56 -0400

    Adding d3 scatterplot

---
+++ Examples/7_D3_scatterplot/scatterplot_lib.js
@@ -0,0 +1,63 @@
+function scatter(div_id, data){
+    // Code adapted from https://www.d3-graph-gallery.com/graph/scatter_animation_start.html
+    // Dataset https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 30, left: 60},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+    // append the svg object to the body of the page
+    var svg = d3.select(div_id)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")")
+
+    // Add X axis
+    var x = d3.scale.linear()
+        .domain([0, 0])
+        .range([ 0, width ]);
+    svg.append("g")
+        .attr("class", "myXaxis axis")   // Note that here we give a class to the X axis, to be able to call it later and modify it
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.svg.axis().scale(x))
+        .attr("opacity", "0")
+
+    // Add Y axis
+    var y = d3.scale.linear()
+        .domain([0, 500000])
+        .range([ height, 0]);
+    svg.append("g")
+        .attr("class", "axis")
+        .call(d3.svg.axis()
+            .orient('left')
+            .scale(y));
+
+    // Add dots
+    svg.append('g')
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) { return x(d.GrLivArea); } )
+        .attr("cy", function (d) { return y(d.SalePrice); } )
+        .attr("r", 1.5)
+        .style("fill", "#69b3a2")
+
+    // new X axis
+    x.domain([0, 4000])
+    svg.select(".myXaxis")
+        .transition()
+        .duration(2000)
+        .attr("opacity", "1")
+        .call(d3.svg.axis().scale(x));
+
+    svg.selectAll("circle")
+        .transition()
+        .delay(function(d,i){return(i*3)})
+        .duration(2000)
+        .attr("cx", function (d) { return x(d.GrLivArea); } )
+        .attr("cy", function (d) { return y(d.SalePrice); } )
+
+}
